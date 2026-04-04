53d7820fc7b066177d868613cff491fd70eaa3b1
fjaramilloo <fjaramilloconsultor@gmail.com>
2026-04-04 06:45:38 -0500

    app ios

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fedejaramilloo.agrogestion',
+  appName: 'Agrogestion',
+  webDir: 'dist'
+};
+
+export default config;
