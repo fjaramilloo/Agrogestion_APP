6e1eb91d4cff14a408671734694dd5591b576925
fjaramilloo <fjaramilloconsultor@gmail.com>
2026-04-06 15:16:54 -0500

    Fix webDir in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,6 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.fedejaramilloo.agrogestion',
   appName: 'Agrogestion',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   },
