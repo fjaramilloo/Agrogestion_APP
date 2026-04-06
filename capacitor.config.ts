22b9de6b8f64e6ba62294e46a7a16e7f5a37bcdf
fjaramilloo <fjaramilloconsultor@gmail.com>
2026-04-06 15:07:28 -0500

    Configure Live Updates (OTA) and set Premium App Icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.fedejaramilloo.agrogestion',
   appName: 'Agrogestion',
-  webDir: 'dist'
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    CapacitorUpdater: {
+      autoUpdate: true,
+      statsUrl: "https://capgo.app/api/stats",
+      resetWhenUpdateFailed: true
+    }
+  }
 };
 
 export default config;
